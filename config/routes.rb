aa418183278652409d46e88208f931bb30ade969
Alex Manelis <amanelis@gmail.com>
2012-12-06 15:58:13 -0800

    Boom full control bitches

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,23 @@ Vacuum::Application.routes.draw do
   get "projects/index"
 
   # Devise model mapping
-  devise_for :users
+  devise_for :users,
+    :controllers => {
+      :confirmations      => 'user/confirmations',
+      :omniauth_callbacks => 'user/omniauth_callbacks',
+      :passwords          => 'user/passwords',
+      :registrations      => 'user/registrations',
+      :sessions           => 'user/sessions',
+      :unlocks            => 'user/unlocks'
+  }
   
   # Devise scoped routes
   devise_scope :user do
-    get '/account'       => 'devise/registrations#edit',  as: 'account'
-    get '/login'         => 'devise/sessions#new',        as: 'login'
-    get '/logout'        => 'devise/sessions#destroy',    as: 'logout'
-    get '/signup'        => 'devise/registrations#new',   as: 'signup'
-    get '/forgot'        => 'devise/passwords#new',       as: 'forgot_password'
+    get '/account'       => 'user/registrations#edit',  as: 'account'
+    get '/login'         => 'user/sessions#new',        as: 'login'
+    get '/logout'        => 'user/sessions#destroy',    as: 'logout'
+    get '/signup'        => 'user/registrations#new',   as: 'signup'
+    get '/forgot'        => 'user/passwords#new',       as: 'forgot_password'
   end
 
   # Standard Resourcing
