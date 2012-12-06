0c139416925594f5d15c590a485f5e78bf4dac50
Alex Manelis <amanelis@gmail.com>
2012-12-06 09:52:11 -0800

    Turned off active_Record

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Vacuum
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
     
     # Turn off asset compliation on rakes
     config.assets.initialize_on_precompile = false
