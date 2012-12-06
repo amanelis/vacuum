f848fdec7fc8c1bf806b2f186dfe612ed5359027
Alex Manelis <amanelis@gmail.com>
2012-12-06 09:49:41 -0800

    Added rspec and zurb

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,10 @@ require 'action_mailer/railtie'
 require 'active_resource/railtie'
 require 'active_record/railtie'
 
+# require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "active_resource/railtie"
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
