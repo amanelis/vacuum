4ae7f0e5f483168f6b969893961e86b25ca85a2c
Alex Manelis <amanelis@gmail.com>
2012-12-10 12:43:18 -0800

    Asset error

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Vacuum
     config.assets.initialize_on_precompile = true
 
     # Enable the asset pipeline
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
