269cfe9303a4e471209de8739b424557dc5dea32
Matt Johnston <matt@tailored.co>
2013-01-10 17:26:51 -0800

    More visual polishing

--- app/models/error.rb
+++ app/models/error.rb
@@ -2,20 +2,20 @@ class Error
   include Mongoid::Document
   include Mongoid::Timestamps
   include DefaultAttributeSetters
-  
+
   field :level,       type: String
   field :message,     type: String
-  
+
   field :resolved,    type: Boolean
   field :count,       type: Integer, default: 0
   field :identifier,  type: String
 
   ### Associations
   belongs_to :project
-  
+
   ### Embedding
   embeds_many :occurrences
-  
+
   # Callbacks
   before_create :set_identifier
 end
--- app/models/occurrence.rb
+++ app/models/occurrence.rb
@@ -2,25 +2,25 @@ class Occurrence
   include Mongoid::Document
   include Mongoid::Timestamps
   include DefaultAttributeSetters
-  
-  
+
+
   field :url,             type: String # file:///Users/arm/Development/Rails/tmp/index.html
   field :parent_url,      type: String # file:///Users/arm/Development/Rails/tmp/index.html
   field :line,            type: String # 17
-  field :user_agent,      type: String # Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11  
+  field :user_agent,      type: String # Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11
   field :app_name,        type: String # Netscape
   field :platform,        type: String # MacIntel
   field :parameters,      type: String # ?api_key=helokey
   field :cookie_enabled,  type: String # true/false
   field :identifier,      type: String # 20318jh30114thgsdlkj3
   field :stack_trace,     type: String
-  
+
   ### Embedding
   embedded_in :error
-  
+
   ### Callbacks
   before_create :set_identifier
-  
+
   # human_agent
   # @param:
   # @return: String[human readable version of the User agent]
