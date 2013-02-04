b37ecbf12dc5a7b45712cf98eebf7e899bee1ecc
Alex Manelis <amanelis@gmail.com>
2013-02-03 16:44:10 -0800

    Added sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Vacuum::Application.initialize!
+
+# Email with sendgrid
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => 'app9868490@heroku.com',
+  :password       => 'j5ubavuh',
+  :domain         => 'vacuum.io'
+}
+ActionMailer::Base.delivery_method ||= :smtp
