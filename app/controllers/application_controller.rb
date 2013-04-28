0384129f2ff88a1edc982d970e31246aa9efdc5d
Alex Manelis <amanelis@gmail.com>
2013-04-28 10:04:35 -0700

    Configured mail options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_filter :mailer_set_url_options
   after_filter :set_access_control_headers
   
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Request-Method'] = '*'
+  end
+  
+  def mailer_set_url_options
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
   end
 end
