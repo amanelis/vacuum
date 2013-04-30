9b5ab5ea26aa500897d1ae0d86b6ab1c8d1454e7
Alex Manelis <amanelis@gmail.com>
2013-04-29 17:32:05 -0700

    Fixed authorization

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,21 +1,35 @@
 class ErrorsController < ApplicationController
-  respond_to :html, :js, :json, :xml
   before_filter :authenticate_user!
+  before_filter :load_project, :only => [:index, :show, :resolve]
+  before_filter :load_error,   :only => [:show, :resolve]
+  respond_to :html, :js 
+  
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = "Access denied for that projects errors."
+    redirect_to root_path
+  end
   
   def index
-    @project = current_user.projects.find(params[:project_id])
+    authorize! :read, @project
     @errors  = @project.errors.desc(:updated_at).paginate(page: params[:page], per: 10)
   end
   
   def show
-    @project     = current_user.projects.find(params[:project_id])
-    @error       = @project.errors.find(params[:id])
+    authorize! :read, @error
     @occurrences = @error.occurrences.desc(:updated_at).paginate(page: params[:page], per: 10)
   end
   
   def resolve
-    @project = current_user.projects.find(params[:project_id])
-    @error   = @project.errors.find(params[:id])
+    authorize! :resolve, @error
     @error.update_attributes!(resolved: params[:error][:resolved])
   end
+  
+  private
+    def load_project
+      @project = current_user.projects.find(params[:project_id])
+    end
+    
+    def load_error
+       @error = @project.errors.find(params[:id])
+    end
 end
