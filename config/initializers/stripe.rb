109eaef4bf746d13061d6a13c2ee25a412058969
Alex Manelis <amanelis@gmail.com>
2013-05-01 23:51:06 -0700

    Fixed env error for defining stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,4 @@
-if Rails.env.development? || Rails.env.production?
+if Rails.env.development? || Rails.env.test?
   STRIPE_SECRET_KEY       = 'roo18oi6q0tKADHttgsZ4FNQXNyDKv7S'
   STRIPE_PUBLISHABLE_KEY  = 'pk_jSXKsaEuajlnOpttqY47lOS5BrZgS'
 elsif Rails.env.production?
