6ca437bce432d2564684a5b878194ce56f6872ac
Alex Manelis <amanelis@gmail.com>
2013-05-11 07:40:39 +0300

    Added seo initializer for head files

---
+++ config/initializers/seo.rb
@@ -0,0 +1,16 @@
+$meta = {
+ keywords: {
+   name: 'keywords',
+   content: 'javascript, javascript errors, front end error logging, javascript error logging, javascript error logging service, js errors, vacuum error logging, vacuum.io, vacuum javascript'
+ },
+ 
+ description: {
+   name: 'description',
+   content: 'Vacuum is a front end error logging and reporting service. Log errors on client side from the browsers of your users.'
+ },
+ 
+ viewport: {
+   name: 'viewport',
+   content: 'width = device-width, initial-scale = 1, user-scalable = no, maximum-scale = 1'
+ }
+}
