2d863d0b74e9021854e2ff3799bfd8374e6149f8
Matt Johnston <matt@tallpixels.com>
2013-05-18 13:54:38 -0700

    Fixed the way I submit the error ids to the resolve route. Also has some weird bundle artifacts

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,33 +2,33 @@ class ErrorsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :load_project, :only => [:index, :show, :resolve]
   before_filter :load_error,   :only => [:show, :resolve]
-  respond_to :html, :js 
-  
+  respond_to :html, :js
+
   rescue_from CanCan::AccessDenied do |exception|
     flash[:alert] = "Access denied for that projects data."
     redirect_to root_path
   end
-  
+
   def index
     authorize! :read, @project
     @errors  = @project.errors.unresolved.desc(:updated_at).paginate(page: params[:page], per: 10)
   end
-  
+
   def show
     authorize! :read, @error
     @occurrences = @error.occurrences.desc(:updated_at).paginate(page: params[:page], per: 10)
   end
-  
+
   def resolve
     authorize! :resolve, @error
     @error.update_attributes!(resolved: params[:error][:resolved])
   end
-  
+
   private
     def load_project
       @project = current_user.projects.find(params[:project_id])
     end
-    
+
     def load_error
        @error = @project.errors.find(params[:id])
     end
