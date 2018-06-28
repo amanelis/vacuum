ee0f8f665c285e43257b062eada5c7d1e7ee822a
Alex Manelis <amanelis@users.noreply.github.com>
2018-06-27 23:30:45 -0600

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,7 @@ Vacuum::Application.configure do
   # config.threadsafe!
   config.action_mailer.delivery_method = :smtp 
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
